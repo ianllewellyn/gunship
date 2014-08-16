ac5233b54fd6d9652d5bf91cf1ea6276a6ceca28
ianllewellyn <ian@designsideways.com>
2014-08-16 10:31:41 +0100

    use requestAnimationFrame() for game loop

--- app/helpers/game_loop.js
+++ app/helpers/game_loop.js
@@ -1,5 +1,10 @@
 (function(){
 	
+	// Get the current time in ms
+	var time = function(){
+		return new Date().getTime();
+	}
+	
 	// A simple game loop
 	//
 	// {
@@ -21,39 +26,47 @@
 		this.height = this.canvas.height;
 		this.assets = [];
 		
+		self = this;
+		
 		// Start the game loop
+		// Initialize the game and draw the initial frame.
 		this.start = function(){
-			this.startTime = new Date().getTime();
-			this.lastFrame = this.startTime;
-			self = this;
-			self.initialize(this.assets);
+			self.lastFrame = time();
+			self.initialize(self.assets);
+			self._drawFrame();
+		}
+		
+		// Draw a frame of the game. Update all assets then draw them.
+		this._drawFrame = function(){
+			var now = time();
+			self.frameTime = frameTime = now-self.lastFrame;
+			self.frameRate = Math.floor(1000/self.frameTime);
+			self.lastFrame = now;
 			
-			//TODO: Use requestAnimationFrame() here instead
-			// https://github.com/ooflorent/js13k-boilerplate/blob/master/src/raf.js
-			setInterval(function(){
-				var now = new Date().getTime();
-				self.frameTime = frameTime = now-self.lastFrame;
-				self.frameRate = Math.floor(1000/self.frameTime);
-				self.lastFrame = now;
-				
-				// Update each of the assets then call the update callback
-				self.assets.forEach(function(asset){
-					if(asset.update){
-						asset.update(frameTime);
-					}
-				});
-				self.update(frameTime);
-				
-				// Clear the canvas before calling draw() on each asset
-				// followed by the draw callback
-				self.ctx.clearRect(0, 0, 400, 400)
-				self.assets.forEach(function(asset){
-					if(asset.draw){
-						asset.draw(self.ctx);
-					}
-				});
-				self.draw(self.ctx);
-			}, 1000/this.fps);
+			// Update each of the assets then call the update callback.
+			// frameTime is passed into the update call so assets know how long
+			// since the last frame was updated and don't have to manage it themselves.
+			self.assets.forEach(function(asset){
+				if(asset.update){
+					asset.update(frameTime);
+				}
+			});
+			// Also call the update callback for non-asset
+			self.update(frameTime);
+			
+			// Clear the canvas before calling draw() on each asset
+			// followed by the draw callback
+			self.ctx.clearRect(0, 0, 400, 400);
+			self.assets.forEach(function(asset){
+				if(asset.draw){
+					asset.draw(self.ctx);
+				}
+			});
+			// Also call the draw callback for non-asset
+			self.draw(self.ctx);
+			
+			// Request the next frame draw
+			requestAnimationFrame(self._drawFrame);
 		}
 	}
 })();
