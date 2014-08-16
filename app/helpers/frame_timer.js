890dc546de066325c4f9ba8a4807a0e39a3c437c
ianllewellyn <ian@designsideways.com>
2014-08-16 11:38:33 +0100

    change from self to this in frame_timer

--- app/helpers/frame_timer.js
+++ app/helpers/frame_timer.js
@@ -3,13 +3,11 @@
 		this._frames = [];
 		this._fps = 0;
 		
-		var self = this;
-		
 		// Calculates FPS by sampling previous frames. Keep trask of the last 30
 		// frameTime values, when we have 30 we take the average and use it to
 		// calculate the frame rate.
 		this.update = function(frameTime){
-			var frames = self._frames;
+			var frames = this._frames;
 			frames.push(frameTime);
 			if(frames.length > 30)
 			{
@@ -18,11 +16,11 @@
 			
 			if(frames.length == 30){
 				var total = 0;
-				var numFrames = self._frames.length;
+				var numFrames = this._frames.length;
 				for(var i=0; i<numFrames; ++i){
 					total += frames[i];
 				}
-				self._fps = Math.round(1000/(total/numFrames));
+				this._fps = Math.round(1000/(total/numFrames));
 				frames.length = 0;
 			}
 		}
@@ -30,7 +28,7 @@
 		this.draw = function(ctx){
 			ctx.fillStyle = '#fff';
 			ctx.font = '12px Arial';
-			ctx.fillText(self._fps+' fps', 2, 12);
+			ctx.fillText(this._fps+' fps', 2, 12);
 		}
 	}
 })();
