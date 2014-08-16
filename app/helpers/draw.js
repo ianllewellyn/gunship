430ef564404fd93796bfeb1802960b11a4c5edad
ianllewellyn <ian@designsideways.com>
2014-08-17 00:02:38 +0100

    added helper with draw functions

---
+++ app/helpers/draw.js
@@ -0,0 +1,43 @@
+(function(){
+	// Helper method to draw a shape from an array of points
+	window.drawShape = function(ctx, points, tx, mirror){
+		if(!tx){
+			tx = {x: 0, y: 0};
+		}
+		if(mirror == undefined){
+			mirror = false;
+		}
+		ctx.beginPath();
+		ctx.moveTo(tx.x+points[0][0], tx.y+points[0][1]);
+	
+		// Each half, first the supplied path, then run through the points
+		// again backwards and draw a mirror.
+		for(var i=1; i<points.length; ++i){
+			ctx.lineTo(tx.x+points[i][0], tx.y+points[i][1]);
+		}
+		if(mirror){
+			for(var i=points.length-1; i>-1; --i){
+				ctx.lineTo(tx.x+(-points[i][0]), tx.y+points[i][1]);
+			}
+		}
+	
+		// Apply line styles
+		// ctx.closePath();
+		ctx.stroke();
+	}
+	
+	// Helper function for drawing circles
+	window.drawCircle = function(ctx, x, y, r){
+		ctx.beginPath();
+		ctx.arc(x, y, r, 0, 2*Math.PI);
+		ctx.stroke();
+	}
+	
+	// Helper function for drawing arcs
+	window.drawArc = function(ctx, x, y, rotation, start, end){
+		ctx.beginPath();
+		ctx.arc(x, y, rotation, start, end);
+		ctx.stroke();
+	}
+	
+})()
