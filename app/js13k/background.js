6103c59e318d57d2f850af10e4eb2c15b9b1025e
ianllewellyn <ian@designsideways.com>
2014-08-25 20:27:26 +0100

    stopped drawing side lines

--- app/js13k/background.js
+++ app/js13k/background.js
@@ -12,12 +12,12 @@
 			ctx.fillRect(0, 0, self.width, self.height);
 			
 			//REMOVE: Draw the bounds lines
-			ctx.beginPath();
-			ctx.moveTo(20, 0);
-			ctx.lineTo(20, self.height);
-			ctx.moveTo(self.width-20, 0);
-			ctx.lineTo(self.width-20, self.height);
-			ctx.stroke();
+			// ctx.beginPath();
+			// ctx.moveTo(20, 0);
+			// ctx.lineTo(20, self.height);
+			// ctx.moveTo(self.width-20, 0);
+			// ctx.lineTo(self.width-20, self.height);
+			// ctx.stroke();
 		}
 	}
 })();
